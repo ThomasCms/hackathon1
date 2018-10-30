3fced958520ace6db20c18079c9cf69ef0909a5b
Thomas Coumes <thomascoumes3145@gmail.com>
2018-10-30 23:50:30 +0100

    restore index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,47 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 11/10/17
- * Time: 14:01
- */
+require '../vendor/autoload.php';
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+
+$client = new GuzzleHttp\Client([
+        'base_uri' => 'https://fr.openfoodfacts.org/categorie/',
+    ]
+);
+$response = $client->request('GET', 'marshmallows.json');
+$body = $response->getBody()->getContents();
+$body = json_decode($body, true);
+
+?>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
 
 
-if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
-    return false;    // retourne la requête telle quelle.
+
+<body>
+
+<h1>Hello, Candy !!!</h1>
+
+<?php
+foreach ($body["products"] as $i => $value) {
+    echo $value["image_url"];
+    echo $value["product_name"];
 }
+?>
 
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../app/db.php';
-require_once __DIR__ . '/../app/config.php';
-require_once __DIR__ . '/../app/dispatcher.php';
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
