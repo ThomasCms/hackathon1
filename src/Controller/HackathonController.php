be2b52fa3414c7c10390c8f0a3aef2cc69a67f1a
Thomas Coumes <thomascoumes3145@gmail.com>
2018-10-31 05:22:32 +0100

    form?

--- src/Controller/HackathonController.php
+++ src/Controller/HackathonController.php
@@ -13,7 +13,7 @@ use GuzzleHttp\ClientInterface;
 class HackathonController extends AbstractController
 {
 
-
+    private $positionDepart;
     /**
      * Display item listing
      *
@@ -26,6 +26,10 @@ class HackathonController extends AbstractController
     {
 //        $itemManager = new MarshmallowManager($this->getPdo());
 //        $items = $itemManager->selectAll();
+        if(isset($_POST)){
+            $this->positionDepart = $_POST['name'];
+            echo $this->positionDepart;
+        }
         $client = new Client([
                 'base_uri' => 'https://fr.openfoodfacts.org/categorie/',
             ]
@@ -35,6 +39,13 @@ class HackathonController extends AbstractController
         $candies = json_decode($body, true);
 
         return $this->twig->render('Hackathon/index.html.twig', ['candies' => $candies['products']]);
+    }
+
+    public function address()
+    {
+        //var_dump($_POST['name']);
+
+        //return $this->twig->render('Hackathon/content.html.twig', ['toto' => $_POST]);
     }
 }
 
